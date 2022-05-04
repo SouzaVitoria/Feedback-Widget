b2d7eb9edf142bd0baea514ec892a32a5fdbe1f6
Vitória Souza <viihsouza161@gmail.com>
2022-05-04 19:46:24 -0300

    added Unit Testing

--- server/src/services/submit-feedback.ts
+++ server/src/services/submit-feedback.ts
@@ -15,6 +15,15 @@ export class SubmitFeedbackService {
 
   async execute(request: SubmitFeedbackServiceRequest) {
     const { type, comment, screenshot } = request;
+
+    if (!type || !comment) {
+      throw new Error('Type is required.')
+    }
+
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Invalid screenshot format.')
+    }
+
     await this.feedbacksRepository.create({ type, comment, screenshot })
     await this.mailAdapter.sendMail({
       subject: 'Novo Feedback',
